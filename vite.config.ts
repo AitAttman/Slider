94adfee2b37f4917094e39119b11e9398f045935
Ahmed Ait Attman <ait-3otman@hotmail.com>
2026-01-22 22:58:20 +0100

    rename dist to docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   plugins: [],
   base: "",
   build: {
-    outDir: "build",
+    outDir: "docs",
     emptyOutDir: true,
     copyPublicDir: true,
     rollupOptions: {
